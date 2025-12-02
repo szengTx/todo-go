73725b2a94eb8c34e5a2f55b4ef9c7a9f72f96c2
sz <shezeng@mails.ccnu.edu.cn>
2025-12-02 22:23:48 +0800

    Add main.go file

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"todo-go/database"
+	"todo-go/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.InitDB()
+
+	r := gin.Default()
+	r.SetHTMLTemplate(template.Must(template.ParseGlob("templates/*.html")))
+
+	// 路由
+	r.GET("/register", handlers.Register)
+	r.POST("/register", handlers.Register)
+	r.GET("/login", handlers.Login)
+	r.POST("/login", handlers.Login)
+	r.GET("/tasks", handlers.TasksPage)
+	r.POST("/tasks/create", handlers.CreateTask)
+	r.POST("/tasks/toggle/:id", handlers.ToggleTask)
+	r.GET("/tasks/delete/:id", handlers.DeleteTask)
+
+	r.GET("/", func(c *gin.Context) {
+		if _, err := c.Cookie("user_id"); err == nil {
+			c.Redirect(http.StatusFound, "/tasks")
+		} else {
+			c.Redirect(http.StatusFound, "/login")
+		}
+	})
+
+	r.Run(":8080")
+}
